80ab462be5225edf2e1413ea5c0d3a65aadb6075
Kevin Minutti <minutti70@gmail.com>
2020-03-20 20:56:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,8 +13,14 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
+  else if (age <=12) {return "You are kid"}
 
+  else (age > 19) {return "You are an adult"}
 }
+
 
 function ternaryTeenager(age) {
 
